efd30aa16b41e0b3b4c5c63772dbbd7b2ceff4e5
Alessandro Tolentino Hernández <a01276919@tec.mx>
2023-05-09 19:30:50 -0600

    Add files via upload

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+
+  constructor(private authService: AuthService,  private router: Router) { }
+  ngOnInit() {
+  }
+
+  isAlumno() {
+    const obj = this.authService.currentUserValue;
+    return obj.role === 'Alumno';
+  }
+
+  logout(){
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
